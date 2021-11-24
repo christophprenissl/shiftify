411307a10266a75a1bab3408642763d7601591fe
christoph.prenissl <christoph.prenissl@st.oth-regensburg.de>
2021-11-24 15:51:51 +0100

    feature auth
    - added uses-permission for internet
    - main activity now instances firebase and other fragments only get instance
    - change label of radio group when radio button is clicked
    - added function for user creation

--- app/src/main/java/com/christophprenissl/shiftify/loggedout/LoginFragment.kt
+++ app/src/main/java/com/christophprenissl/shiftify/loggedout/LoginFragment.kt
@@ -5,18 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.christophprenissl.shiftify.R
 import com.christophprenissl.shiftify.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentLoginBinding.inflate(inflater, container, false)
-        val navController = findNavController()
+        navController = findNavController()
 
         binding.loginButton.setOnClickListener {
             //navController.navigate(R.id.action_loginFragment_to_nurseShiftsFragment)
@@ -27,5 +37,14 @@ class LoginFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            navController.navigate(R.id.action_loginFragment_to_nurseShiftsFragment)
+        }
     }
 }
--- app/src/main/java/com/christophprenissl/shiftify/loggedout/RegisterFragment.kt
+++ app/src/main/java/com/christophprenissl/shiftify/loggedout/RegisterFragment.kt
@@ -5,19 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.christophprenissl.shiftify.R
 import com.christophprenissl.shiftify.databinding.FragmentRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
+import timber.log.Timber.i
 
 class RegisterFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        auth = FirebaseAuth.getInstance()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +22,47 @@ class RegisterFragment : Fragment() {
     ): View {
         val binding = FragmentRegisterBinding.inflate(inflater, container, false)
         val navController = findNavController()
+        auth = FirebaseAuth.getInstance()
+
+        binding.identityRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.nurse_radio_button -> {
+                    binding.stationIdLabel.text = getString(R.string.station_code_label)
+                }
+                R.id.shift_owner_button -> {
+                    binding.stationIdLabel.text = getString(R.string.station_name_label)
+                }
+            }
+        }
 
         binding.registerButton.setOnClickListener {
+            val email = binding.emailEdit.text.toString()
+            val firstName = binding.firstNameEdit.text.toString()
+            val lastName = binding.lastNameEdit.text.toString()
+            val password1 = binding.password1Edit.text.toString()
+            val password2 = binding.password2Edit.text.toString()
+            val isNurse = binding.identityRadioGroup.checkedRadioButtonId == R.id.nurse_radio_button
+            val stationEdit = binding.stationIdEdit.text.toString()
+
+
+            if (password1 != password2 && password1.length < 6) {
+                Toast.makeText(context, "Passwords don't match.",
+                    Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+            auth.createUserWithEmailAndPassword(email, password1)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser?.email ?: "user"
+                        i("$user was signed up correctly")
+                        navController.navigate(R.id.action_registerFragment_to_nurseShiftsFragment)
+                    } else {
+                        Toast.makeText(context, "Authentication failed. ${task.result}",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
 
         return binding.root
